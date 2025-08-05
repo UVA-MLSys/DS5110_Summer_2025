e24da899da6bb2c651f331a80b61b2c5c50ae374
BardiaNikpour <157668628+BardiaNikpour@users.noreply.github.com>
2025-08-05 18:53:02 -0400

    Add files via upload

---
+++ Teams/team3/Codes/StateMachinePayload.py
@@ -0,0 +1,11 @@
+{
+  "bucket": "team3-cosmical",
+  "file_limit": "517",
+  "batch_size": 512,
+  "object_type": "folder",
+  "S3_object_name": "Anomaly Detection",
+  "script": "/tmp/Anomaly Detection/Inference/inference.py",
+  "result_path": "resultsstep4/25MB",
+  "data_bucket": "team3-cosmical",
+  "data_prefix": "25MB"
+}
